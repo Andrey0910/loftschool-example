bbc57af1e226f6390f97c575434ea68ea0eb860b
Andrey Petrov <petrovaa71@mail.ru>
2020-05-09 18:17:15 +0300

    сделал дз 5

---
+++ src/dnd.js
@@ -0,0 +1,123 @@
+/* Задание со звездочкой */
+
+/*
+ Создайте страницу с кнопкой.
+ При нажатии на кнопку должен создаваться div со случайными размерами, цветом и позицией на экране
+ Необходимо предоставить возможность перетаскивать созданные div при помощи drag and drop
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+
+/*
+ Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
+ Функция должна только создавать элемент и задвать ему случайные размер/позицию/цвет
+ Функция НЕ должна добавлять элемент на страницу. На страницу элемент добавляется отдельно
+
+ Пример:
+   const newDiv = createDiv();
+   homeworkContainer.appendChild(newDiv);
+ */
+function createDiv() {
+    let div;
+    let maxWidth = 100;
+    let minWidth = 0;
+    let maxHeight = 200;
+    let minHeight = 0;
+    let minY = 0;
+    let maxY = 1080;
+    let minX = 0;
+    let maxX = 1920;
+    let width;
+    let height;
+    let x;
+    let y;
+
+    let randomValue = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
+    let randomColor = () => {
+        let simvols = '0123456789ABCDEF';
+        let color = '#';
+
+        for (let i = 0; i < 6; i++) {
+            color += simvols[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    };
+
+    div = document.createElement('div');
+    div.classList.add('draggable-div');
+
+    width = randomValue(minWidth, maxWidth);
+    height = randomValue(minHeight, maxHeight);
+    x = randomValue(minX, maxX);
+    y = randomValue(minY, maxY);
+
+    div.style.width = width + 'px';
+    div.style.height = height + 'px';
+    div.style.top = x + 'px';
+    div.style.left = y + 'px';
+
+    div.style.background = randomColor();
+    div.style.position = 'relative';
+
+    return div;
+}
+
+/*
+ Функция должна добавлять обработчики событий для перетаскивания элемента при помощи drag and drop
+
+ Пример:
+   const newDiv = createDiv();
+   homeworkContainer.appendChild(newDiv);
+   addListeners(newDiv);
+ */
+function addListeners(target) {
+    target.addEventListener('mousedown', () => {
+
+        target.style.position = 'absolute';
+        target.style.zIndex = '1000';
+
+        let moveAt = (pageX, pageY) => {
+            target.style.left = pageX - target.offsetWidth / 2 + 'px';
+            target.style.top = pageY - target.offsetHeight / 2 + 'px';
+        };
+
+        let onMouseMove = (e) => moveAt(e.pageX, e.pageY);
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        target.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', onMouseMove);
+            target.onmouseup = null;
+        });
+
+    });
+}
+
+let addDivButton = homeworkContainer.querySelector('#addDiv');
+
+addDivButton.addEventListener('click', function() {
+    // создать новый div
+    const div = createDiv();
+
+    // добавить на страницу
+    homeworkContainer.appendChild(div);
+    // назначить обработчики событий мыши для реализации D&D
+    addListeners(div);
+    // можно не назначать обработчики событий каждому div в отдельности, а использовать делегирование
+    // или использовать HTML5 D&D - https://www.html5rocks.com/ru/tutorials/dnd/basics/
+});
+
+export {
+    createDiv
+};
